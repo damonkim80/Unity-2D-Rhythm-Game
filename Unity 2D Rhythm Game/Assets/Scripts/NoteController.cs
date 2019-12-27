303a1319e77a5b130630e36fe00f1ceca055aac4
damonkim80 <42937396+damonkim80@users.noreply.github.com>
2019-12-27 19:17:39 +0900

    노트 오브젝트 풀링 처리

--- Unity 2D Rhythm Game/Assets/Scripts/NoteController.cs
+++ Unity 2D Rhythm Game/Assets/Scripts/NoteController.cs
@@ -16,20 +16,36 @@ public class NoteController : MonoBehaviour
         }
     }
 
-    public GameObject[] Notes;
+    public GameObject[] Notes; //실제 Prefab 으로 만들어진 4개의 노트 오브젝트
+
+    private ObjectPooler noteObjectPooler;
     private List<Note> notes = new List<Note>();
+    private float x, z, startY = 8.0f;
     private float beatInterval = 1.0f;
+
+    void MakeNote(Note note)
+    {
+        GameObject obj = noteObjectPooler.getObject(note.noteType);
+        // 설정된 시작 라인으로 노트를 이동시킵니다.
+        x = obj.transform.position.x;
+        z = obj.transform.position.z;
+        obj.transform.position = new Vector3(x, startY, z);
+        obj.GetComponent<NoteBehavior>().Initialize();
+        obj.SetActive(true);
+    }
 
     IEnumerator AwaitMakeNote(Note note)
     {
         int noteType = note.noteType;
         int order = note.order;
         yield return new WaitForSeconds(order * beatInterval);
-        Instantiate(Notes[noteType - 1]);
+        MakeNote(note);
     }
 
     void Start()
     {
+        noteObjectPooler = gameObject.GetComponent<ObjectPooler>(); //변수 초기화
+
         notes.Add(new Note(1, 1));
         notes.Add(new Note(2, 2));
         notes.Add(new Note(3, 3));
